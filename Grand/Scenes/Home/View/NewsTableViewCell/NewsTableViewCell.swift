8f8902f49a59253c03900df4be8876d17898176c
Morsy Mohamed Elsokary <morsysoker@gmail.com>
2022-07-21 00:58:31 +0200

    Add NewsTableViewCell

---
+++ Grand/Scenes/Home/View/NewsTableViewCell/NewsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTableViewCell.swift
+//  Grand
+//
+//  Created by MorsyElsokary on 21/07/2022.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
