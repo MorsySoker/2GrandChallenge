e49aff59d029f2e9dc1200ce5b11e6160ece4894
Morsy Mohamed Elsokary <morsysoker@gmail.com>
2022-07-22 22:00:06 +0200

    add HeadLinesService

---
+++ Grand/Service/Remote/Headlines/HeadLinesService.swift
@@ -0,0 +1,32 @@
+//
+//  HeadLinesService.swift
+//  Grand
+//
+//  Created by MorsyElsokary on 22/07/2022.
+//
+
+import Foundation
+
+final class HeadLinesService {
+    
+    // MARK: - Properties
+    
+    private var networkServices: NetworkServiceProtocol
+    
+    // MARK: - init
+    
+    init(networkServices: NetworkServiceProtocol = NetworkService()) {
+        self.networkServices = networkServices
+    }
+    
+    func getHeadlines(
+        page: String = "1",
+        country: String = "us",
+        completion: @escaping (Result<ArticleStatus, NetworkError>) -> Void){
+        let endPoint =
+            HeadlinesEndPoint.getHeadLines(
+                page: page, country: country)
+        let request = endPoint.createRequest()
+        networkServices.request(request, completion: completion)
+    }
+}
---
+++ Grand/Service/Remote/Headlines/HeadlinesEndPoint.swift
@@ -0,0 +1,46 @@
+//
+//  HeadlinesEndPoint.swift
+//  Grand
+//
+//  Created by MorsyElsokary on 22/07/2022.
+//
+
+import Foundation
+
+enum HeadlinesEndPoint {
+    
+    case getHeadLines(page: String, country: String)
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .getHeadLines: return .GET
+        }
+    }
+    
+    var requestBody: Encodable? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+    
+    func getURL() -> String {
+        switch self {
+        case .getHeadLines(let page, let country):
+            return "https://newsapi.org/v2/top-headlines?apiKey=\(Enviroment.newsApiKey)&country=\(country)&page=\(page)"
+        }
+    }
+    
+    func createRequest(token: String? = nil) -> NetworkRequest {
+        var headers: Headers = [:]
+        headers["Content-Type"] = "application/json"
+        if let token = token {
+            headers["Authorization"] = "Bearer \(token)"
+        }
+        return NetworkRequest(url: getURL(),
+                              headers: headers,
+                              requestBody: requestBody,
+                              httpMethod: httpMethod)
+    }
+    
+}
