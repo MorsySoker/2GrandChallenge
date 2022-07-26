5e224d1ae97b83361e896082c13706918c07cd97
Morsy Mohamed Elsokary <morsysoker@gmail.com>
2022-07-26 23:35:22 +0200

    add NibLoadingView

---
+++ Grand/Scenes/HeadLines/View/HeadLine Categories/HeadLineCategories.swift
@@ -0,0 +1,15 @@
+//
+//  HeadLineCategories.swift
+//  Grand
+//
+//  Created by MorsyElsokary on 26/07/2022.
+//
+
+import UIKit
+
+class HeadLineCategories: NibLoadingView {
+
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var categoriesCollection: UICollectionView!
+}
